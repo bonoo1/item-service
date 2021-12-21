ed921c269441b400bc015fd68c2b66c5549e0605
bonoo1 <87068832+bonoo1@users.noreply.github.com>
2021-12-22 01:35:03 +0900

    상품 수정

--- src/main/java/hello/itemservice/domain/item/Item.java
+++ src/main/java/hello/itemservice/domain/item/Item.java
@@ -12,13 +12,16 @@ public class Item {
     private Integer price;
     private Integer quantity;
 
-    public Item(Long id) {
-        this.id = id;
+    public Item() {
+
     }
+
+
 
     public Item(String itemName, Integer price, Integer quantity) {
         this.itemName = itemName;
         this.price = price;
         this.quantity = quantity;
     }
+
 }
